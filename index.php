49064ca6bb7d95f0f35afdd4f7335f2400d93099
nandan parekh <nandanparekh52@gmail.com>
2026-01-19 09:02:44 +0530

    seven commit

--- index.php
+++ index.php
@@ -6,13 +6,15 @@
     <title>Javascript 7:30</title>
 </head>
 <body>
-    <p>This is GitHub command lines.</p>
+    <p>This is GitHub command lines start from here.</p>
 
     <!-- 
         git add .
         git commit -m"first commit"  (Note : "first commit" : we can write anything on this message.)
         git push origin main
     -->
-        
+
+    <p>This is GitHub command lines end from here.</p>
+    
 </body>
 </html>
