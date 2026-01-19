5ba21b2623b2168330a6857fd1dd005de6c70b4d
nandan parekh <nandanparekh52@gmail.com>
2026-01-19 08:46:08 +0530

    second commit

--- index.php
+++ index.php
@@ -3,9 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Javascript 7:30</title>
 </head>
 <body>
-    
+    <p>This is Javascript Lecture Classes.</p>
+
+    <!-- 
+        git add .
+        git commit -m"first commit"  (Note : "first commit" : we can write anything on this message.)
+        git push origin main
+    -->
 </body>
 </html>
