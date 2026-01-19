1d34df326994a1839ce3f0c52aea2852f716f458
nandan parekh <nandanparekh52@gmail.com>
2026-01-19 08:57:46 +0530

    fourth commit

--- index.php
+++ index.php
@@ -6,12 +6,13 @@
     <title>Javascript 7:30</title>
 </head>
 <body>
-    <p>This is Javascript Lecture Classes.</p>
+    <p>This is GitHub command lines.</p>
 
     <!-- 
         git add .
         git commit -m"first commit"  (Note : "first commit" : we can write anything on this message.)
         git push origin main
     -->
+        
 </body>
 </html>
